faef6bc3db5e8c64855d64030094d3b0f6abf7f0
Charlie <charliekelly31415@gmail.com>
2019-05-09 14:21:37 -0400

    Solution

--- utils/cart.js
+++ utils/cart.js
@@ -4,15 +4,72 @@ class Cart {
     constructor() {
         if (!Cart.singleCart) {
             this._data = {
-                // store products
+                count: 0,
+                value: 0,
+                items: []
             }
             Cart.singleCart = this
         }
         return Cart.singleCart
     }
-    
+
     addToCart(product, quantity) {
-        // TODO
+      console.log('Adding to cart')
+      // create promise
+      return new Promise((resolve, reject) => {
+        if (product && quantity) {
+          console.log('Product: ' + product.id)
+          // check if product is in stock
+          Avail.inStock(product)
+            .then(isInStock => {
+              if (isInStock) {
+                // update cart
+                console.log('Product is in stock, adding to cart')
+                resolve(this.updateCart(product, quantity))
+              } else {
+                console.log('Product out of stock.')
+                reject(false)
+              }
+            })
+            .catch(() => {
+              console.log('Out of stock')
+              reject(false)
+            })
+        } else {
+          console.log('incomplete parameters')
+          reject(this.getCartText())
+        }
+      })
+    }
+
+    // add product to cart, return current count/value
+    updateCart(product, quantity) {
+        if (typeof product === 'undefined' || typeof quantity === 'undefined') {
+          return this.getCartText()
+        } else {
+          const item = {
+            id: product.id,
+            qty: parseInt(quantity)
+          }
+          this._data.items.push(item)
+          this._data.count += item.qty
+          this._data.value += (item.qty * product.price)
+          this.logCartContents()
+          return this.getCartText()
+        }
+    }
+
+    // Log each product in current cart
+    logCartContents() {
+      console.log('CART CONTENTS ')
+      this._data.items.forEach(function(item) {
+        console.log(`${item.id}: ${item.qty}`)
+      })
+    }
+
+    // text value of cart
+    getCartText() {
+      return this._data.count + ' | $' + Number.parseFloat(this._data.value).toFixed(2)
     }
 }
 
